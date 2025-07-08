222259f95514feb6d7cb740cb65f5d0d1f0fd326
datpham0412 <tiendat041202@gmail.com>
2025-07-08 14:08:10 +1000

    update work email

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,14 +1,14 @@
 export const SOCIAL_LINKS = {
   github: "https://github.com/datpham0412",
   linkedin: "https://www.linkedin.com/in/dat-pham-4b79b0219/",
-  email: "tiendat041202@gmail.com",
+  email: "datpham0412.work@gmail.com",
 } as const
 
 export const PERSONAL_INFO = {
   name: "Dat Pham",
   title: "Software Engineer",
   location: "Melbourne, Australia",
-  email: "tiendat041202@gmail.com",
+  email: "datpham0412.work@gmail.com",
   linkedin: "linkedin.com/in/dat-pham-4b79b0219",
   github: "github.com/datpham0412",
 } as const
